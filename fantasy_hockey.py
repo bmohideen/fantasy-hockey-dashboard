6151ac963401db2d60c2edc43b5c834347855401
bmohideen <70986859+bmohideen@users.noreply.github.com>
2025-01-27 18:10:27 -0500

    Parsing through team data in league rankings, and saving under a specific dataframe

--- fantasy_hockey.py
+++ fantasy_hockey.py
@@ -26,3 +26,24 @@ season_id = "2025"
 team_id = "8"
 espn_s2 = "AECrbCxSDYAsEOTBJQryjbMBUKLBexs8d8uHArXqmeHB6HbBUk0QgHCLiHkFCbixlYN9bIkaaF0W%2BNVR%2BXfeGqhZkR1zkEvUiS8aJOx61mq1KpxsXXiyg9V8Jzsk1A051FjurJUvSfOHv2WjgAeh0Mjxx%2FG%2FeLQYk8k0VJ1WsvOs6mf9UHEizoelxAnuC%2BiVYlL7GoIXIZnHwONkl2ml1I6Af1f%2FsyBfaVA35UXRHHD9lVJ8t92PHsKAIYiYlRYAqkO7I54rNdls%2FF2GttcL%2B242ARh86GCgsiewXprufIEmtQ%3D%3D" 
 swid = "{EC688D8E-6470-412F-8C38-809400D9316B}"
+
+# parsing through team data in league rankings
+# if statement so this only runs if fetching team data is successful
+if teams:
+    def parse_team_data(teams):
+        team_stats = []
+        for team in teams.get("teams", []):
+            team_stats.append({
+                "Team Name": team.get("name"),
+                "Abbreviation": team.get("abbrev"),
+                "Projected Rank": team.get("currentProjectedRank"),
+                "Points": team.get("points"),
+                "Wins": team.get("record", {}).get("overall", {}).get("wins"),
+                "Losses": team.get("record", {}).get("overall", {}).get("losses"),
+                "Winning Percentage": team.get("record", {}).get("overall", {}).get("percentage"),
+                "Logo": team.get("logo"),
+            })
+        return pd.DataFrame(team_stats)
+
+# save all team data under a specific dataframe
+    team_data_df = parse_team_data(teams)
